f2c1703b1ef101833b42a4f0f27ac9341a22b1e5
Jonas Thomsen <jonastoftethomsen@gmail.com>
2018-02-27 13:31:55 +0100

    Tilføjede UUID'er til alle events

--- src/automat/Event.java
+++ src/automat/Event.java
@@ -2,57 +2,60 @@
 package automat;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class Event {
-    private Date Dato;
+    private Date Dato = new Date();
+    private UUID uuid = UUID.randomUUID();
     private String event;
     private double var;
     private String tekst;
     private int valg;
     
+    
+    
     public Event(String event, double var) {
-        Dato = new Date();
         this.event = event;
         switch(event) {
             case "indsæt penge" : {
                 valg = 1;
-                tekst = Dato.toString() + " Der er indsat " + var +" kr.";
+                tekst = uuid.toString() + " " + Dato.toString() + " Der er indsat " + var +" kr.";
                 break;
             } case "penge retur": {
                 valg = 2;
-                tekst = Dato.toString() + " Der er returneret" + var + " kr.";
+                tekst = uuid.toString() + " " + Dato.toString() + " Der er returneret" + var + " kr.";
                 break;
             } case "print billet": {
                 valg = 3;
-                tekst = Dato.toString() + " Der printes en billet til " + var + " kr.";
+                tekst = uuid.toString() + " " + Dato.toString() + " Der printes en billet til " + var + " kr.";
                 break;
             } case "admin login": {
                 valg = 4;
-                tekst = Dato.toString() + " Admin logget ind.";
+                tekst = uuid.toString() + " " + Dato.toString() + " Admin logget ind.";
                 break;
             } case "admin logud": {
                 valg = 5;
-                tekst = Dato.toString() + " Admin logget ud.";
+                tekst = uuid.toString() + " " + Dato.toString() + " Admin logget ud.";
                 break;
             } case "admin forsøg": {
                 valg = 6;
-                tekst = Dato.toString() + " Forsøgt Admin login.";
+                tekst = uuid.toString() + " " + Dato.toString() + " Forsøgt Admin login.";
                 break;
             } case "reset": {
                 valg = 7;
-                tekst = Dato.toString() + " Automat reset.";
+                tekst = uuid.toString() + " " + Dato.toString() + " Automat reset.";
                 break;
             } case "billetpris sat": {
                 valg = 8;
-                tekst = Dato.toString() + " Billetpris er sat til" + var + " kr.";
+                tekst = uuid.toString() + " " + Dato.toString() + " Billetpris er sat til" + var + " kr.";
                 break;
             } case "manglende tilladelse": {
                 valg = 8;
-                tekst = Dato.toString() + " Der er frsøgt at tilgå montrør funktion ID:" + var;
+                tekst = uuid.toString() + " " + Dato.toString() + " Der er frsøgt at tilgå montrør funktion ID:" + var;
                 break;
             } default: {
                 valg = 0;
-                tekst = Dato.toString() + " Fejl i LOG!!";
+                tekst = uuid.toString() + " " + Dato.toString() + " Fejl i LOG!!";
                 break;
             }
             
