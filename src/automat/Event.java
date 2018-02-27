818d1c4f0f93fc59fa8508b985bb297a2beba203
Jonas Thomsen <jonastoftethomsen@gmail.com>
2018-02-27 11:40:33 +0100

    Lavede Switch til Event klassen.
    Tilføjede de valg som event loggen pt skal kunne håndtere.

--- src/automat/Event.java
+++ src/automat/Event.java
@@ -7,11 +7,50 @@ public class Event {
     private Date Dato;
     private String event;
     private double var;
+    private String tekst;
+    private int valg;
     
     public Event(String event, double var) {
         Dato = new Date();
         this.event = event;
-        switch(handling) {
+        switch(event) {
+            case "indsæt penge" : {
+                valg = 1;
+                tekst = Dato.toString() + "Der er indsat " + var +" kr.";
+                break;
+            } case "penge retur": {
+                valg = 2;
+                tekst = Dato.toString() + "Der er returneret" + var + " kr.";
+                break;
+            } case "print billet": {
+                valg = 3;
+                tekst = Dato.toString() + "Der printes en billet til " + var + " kr.";
+                break;
+            } case "admin login": {
+                valg = 4;
+                tekst = Dato.toString() + "Admin logget ind.";
+                break;
+            } case "admin logud": {
+                valg = 5;
+                tekst = Dato.toString() + "Admin logget ud.";
+                break;
+            } case "admin forsøg": {
+                valg = 6;
+                tekst = Dato.toString() + "Forsøgt Admin login.";
+                break;
+            } case "reset": {
+                valg = 7;
+                tekst = Dato.toString() + "Automat reset.";
+                break;
+            } case "billetpris sat": {
+                valg = 8;
+                tekst = Dato.toString() + "Billetpris er sat til" + var + " kr.";
+                break;
+            } default: {
+                valg = 0;
+                tekst = Dato.toString() + "Fejl i LOG!!";
+                break;
+            }
             
         }
     }
