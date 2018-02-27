966a1ec3cfe9b3b78482b516cd533794552fe667
Jonas Thomsen <jonastoftethomsen@gmail.com>
2018-02-27 13:02:53 +0100

    Opsætning i event teksterne

--- src/automat/Event.java
+++ src/automat/Event.java
@@ -16,43 +16,43 @@ public class Event {
         switch(event) {
             case "indsæt penge" : {
                 valg = 1;
-                tekst = Dato.toString() + "Der er indsat " + var +" kr.";
+                tekst = Dato.toString() + " Der er indsat " + var +" kr.";
                 break;
             } case "penge retur": {
                 valg = 2;
-                tekst = Dato.toString() + "Der er returneret" + var + " kr.";
+                tekst = Dato.toString() + " Der er returneret" + var + " kr.";
                 break;
             } case "print billet": {
                 valg = 3;
-                tekst = Dato.toString() + "Der printes en billet til " + var + " kr.";
+                tekst = Dato.toString() + " Der printes en billet til " + var + " kr.";
                 break;
             } case "admin login": {
                 valg = 4;
-                tekst = Dato.toString() + "Admin logget ind.";
+                tekst = Dato.toString() + " Admin logget ind.";
                 break;
             } case "admin logud": {
                 valg = 5;
-                tekst = Dato.toString() + "Admin logget ud.";
+                tekst = Dato.toString() + " Admin logget ud.";
                 break;
             } case "admin forsøg": {
                 valg = 6;
-                tekst = Dato.toString() + "Forsøgt Admin login.";
+                tekst = Dato.toString() + " Forsøgt Admin login.";
                 break;
             } case "reset": {
                 valg = 7;
-                tekst = Dato.toString() + "Automat reset.";
+                tekst = Dato.toString() + " Automat reset.";
                 break;
             } case "billetpris sat": {
                 valg = 8;
-                tekst = Dato.toString() + "Billetpris er sat til" + var + " kr.";
+                tekst = Dato.toString() + " Billetpris er sat til" + var + " kr.";
                 break;
             } case "manglende tilladelse": {
                 valg = 8;
-                tekst = Dato.toString() + "Der er frsøgt at tilgå montrør funktion ID:" + var;
+                tekst = Dato.toString() + " Der er frsøgt at tilgå montrør funktion ID:" + var;
                 break;
             } default: {
                 valg = 0;
-                tekst = Dato.toString() + "Fejl i LOG!!";
+                tekst = Dato.toString() + " Fejl i LOG!!";
                 break;
             }
             
