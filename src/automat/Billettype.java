fc1a9dcd02e26231c368ab19dcba87a844673c65
Jonas Thomsen <jonastoftethomsen@gmail.com>
2018-03-01 22:52:59 +0100

    Tilføjede funktioner til Billettype klassen.

--- src/automat/Billettype.java
+++ src/automat/Billettype.java
@@ -1,6 +1,22 @@
 package automat;
 
-public class Billettype {
-    double pris;    
-    String navn;
-}
+    public class Billettype {
+        private double pris;    
+        private String navn;
+        
+        
+        public double getBilletpris() {
+            return pris;
+        }
+        
+        public String getNavn(){
+            return navn;
+        }
+        
+        public void PrintBillet(int antal){
+            for(int i = 0; i < antal; i++) {
+                System.out.println("PRINT BILLETER??");
+            }
+        }
+    }
+
