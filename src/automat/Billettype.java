b68b631d711825cdd6dcff3484ae4bdcf6240cdd
Jonas Thomsen <jonastoftethomsen@gmail.com>
2018-03-05 11:10:25 +0100

    Constructor og funktioner til billettype.java

--- src/automat/Billettype.java
+++ src/automat/Billettype.java
@@ -4,6 +4,18 @@ package automat;
         private double pris;    
         private int zoner;
         
+        public Billettype(double inPris, int inZoner) {
+            pris = inPris;
+            zoner = inZoner;
+        }
+        
+        public void setBilletpris(double nyPris) {
+            pris = nyPris;
+        }
+        
+        public void setZoner(int nyZoner) {
+            zoner = nyZoner;
+        }       
         
         public double getBilletpris() {
             return pris;
