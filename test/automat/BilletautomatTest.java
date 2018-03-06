bcdf8824e895297ce64217d6e48cd8561ac62a39
Jonas Thomsen <jonastoftethomsen@gmail.com>
2018-03-06 09:57:33 +0100

    Opdaterede Junit testen

--- test/automat/BilletautomatTest.java
+++ test/automat/BilletautomatTest.java
@@ -1,45 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package automat;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Jonas
- */
 public class BilletautomatTest {
     
-    public BilletautomatTest() {
-    }
-    
-    
-    /**
-     * Test of getBilletpris method, of class Billetautomat.
-     */
     @Test
     public void test_IndsætPenge() {
         Billetautomat instance = new Billetautomat();
         
         instance.indsætPenge(-100);
         assertEquals(0, instance.getBalance(), 0.0001);
+        
         instance.indsætPenge(100);
         assertEquals(100, instance.getBalance(), 0.0001);
+        
         instance.indsætPenge(-50);
         assertEquals(100, instance.getBalance(), 0.0001);
         
-        
         instance.indsætPenge(0);
         instance.indsætPenge(10);
+        assertEquals(110, instance.getBalance(), 0.0001);
+        
         instance.indsætPenge(0.5);
+        assertEquals(110.5, instance.getBalance(), 0.0001);
+        
+        
         instance.indsætPenge(250.5);
-       
-        assertEquals((100 + 0 + 10+ 0.5+ 250.5), instance.getBalance(), 0.0001);
+        assertEquals(361, instance.getBalance(), 0.0001);
     }
+    
+    @Test
+    public void test_pengeRetur() {
+        
+    }
+    
+    
 }
 
     
